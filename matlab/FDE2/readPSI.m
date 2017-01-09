9f8905876e283f08c6a72eb716ff9046cbaeaf7f
Jeff Ravert <jravert88@gmail.com>
2017-01-09 13:19:26 -0700

    All the equations are put into the equation sections, the sections havent been revised

--- matlab/FDE2/readPSI.m
+++ matlab/FDE2/readPSI.m
@@ -1,11 +1,13 @@
 fileID = fopen('FreqTestFileFromMatlabPSI_r.txt','r');
 formatSpec = '%f';
 A = fscanf(fileID,formatSpec);
-whos A
+% whos A
 fclose(fileID);
 myAxis = -2:4/length(A):2-1/length(A);
-plot(myAxis,10*log10((fftshift(A))))
-whos myAxis 
+
+figure(1); clf;
+plot(myAxis,10*log10((fftshift(A)))-4,'--')
+% whos myAxis 
 
 load('RR1.mat')
 % rr = ifft(RR);
@@ -13,10 +15,25 @@ load('RR1.mat')
 % RR = fft(RR);
 hold on
 myAxis = -4:8/length(RR):4-1/length(RR);
-plot(myAxis,10*log10(fftshift(RR))-65)
+plot(myAxis,10*log10(fftshift(RR))-65-5)
 grid on
 hold off
 
 marge = axis;
-axis([-2 2 marge(3:4)])
+axis([-2 2 -50 10])
+
+latexWidth = 5;
+latexHeight = 4;
+ff = gcf;
+homer = ff.Units;
+ff.Units = 'inches';
+bart = ff.Position;
+ff.Position = [bart(1:2) latexWidth latexHeight];
+ff.PaperPositionMode = 'auto';
+ff.Units = homer;
+xlabel('cycles/bit')
+ylabel('Magintude (dB)')
+% title('SOQPSK-TG Constellation')
+legend('\Psi','SOQPSK Power Spectrum','Location','South')
+print(ff, '-depsc', ['file_name']) %save as eps a 
 
