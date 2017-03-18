15fccb86f11f1467ca5d6c3c7349bc796ea75ab7
Jeff Ravert <jravert88@gmail.com>
2017-03-18 03:07:49 -0600

    Built Batched convolution code

--- matlab/gpu_intro/minWindows.m
+++ matlab/gpu_intro/minWindows.m
@@ -1,11 +1,13 @@
-function [ minSignal, minx ] = minWindows(x,signal,width)
+function [ out_data, out_x ] = minWindows(x,data,myWindowLength)
+numWindows = floor(length(data)/myWindowLength);
 
-minSignal = [];
-minx = [];
-for i = 1:floor(length(signal)/width)-1
-    [A I] = min(signal(i*width+1:(i+1)*width));
-    minSignal = [minSignal; A];
-    minx = [minx; i*width+I];
+out_x = zeros(1,numWindows);
+out_data = zeros(1,numWindows);
+for numWin = 1:numWindows
+    myWindow = (numWin-1)*myWindowLength+1:numWin*myWindowLength;
+    [A I] = min(data(myWindow));
+    out_data(numWin)= A;
+    out_x(numWin)   = x(min(myWindow)+I-1);
 end
 end
 
